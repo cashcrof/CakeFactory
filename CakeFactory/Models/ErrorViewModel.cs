633f8e85a2445b9ab942c7d6719a7ee9f64f9a71
Ceilidh Ashcroft <ceilinatrix@gmail.com>
2022-11-30 09:52:39 -0800

    Added base project files

---
+++ CakeFactory/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CakeFactory.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
